5ba7d4d7d52b0a957d6e1a155d9cbdb500525ac1
th3eng <ahmedfarag01111@gmail.com>
2022-06-19 15:51:01 +0200

    created a main challenges page list

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('<month>', views.months),
+    path('<int:month>', views.months_by_num),
+    # name='months' to use in the reverse function in the views file
+    #  to get the url path of the month challenge page automatically. eg:challenge/january
+    path('<str:month>', views.months, name='challenges_path'),
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,31 +1,65 @@
+from tkinter import N
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
-months_list = [
-    'january',
-    'february',
-    'march',
-    'spril',
-    'may',
-    'june',
-    'july',
-    'august',
-    'september',
-    'october',
-    'november',
-    'december',
-]
-
-months_challenge_text = ['Challenge 1', 'Challenge 2', 'Challenge 3', 'Challenge 4', 'Challenge 5', 'Challenge 6',
-                         'Challenge 7', 'Challenge 8', 'Challenge 9', 'Challenge 10', 'Challenge 11', 'Challenge 12', ]
+# monthly challenges dictionary
+months_list = {
+    'january': 'Play the game of life.',
+    'february': 'Write a program that prints out the numbers from 1 to 100.',
+    'march': 'Watch the movie "The Matrix".',
+    'april': 'sleep for at least 5 hours.',
+    'may': 'Write a program that prints out the numbers from 1 to 100.',
+    'june': 'Play the game of life.',
+    'july': 'Write a program that prints out the numbers from 1 to 100.',
+    'august': 'Watch the movie "The Matrix".',
+    'september': 'sleep for at least 5 hours.',
+    'october': 'Write a program that prints out the numbers from 1 to 100.',
+    'november': 'Play the game of life.',
+    'december': 'Write a program that prints out the numbers from 1 to 100.',
+}
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the monthly_challenges index.")
+    months = ''
+    for month in list(months_list.keys()):
+        months += f"<li><a href='{reverse('challenges_path', kwargs={'month': month})}'>{month}</a></li>"
+
+    response_data = f"<ul>{months}</ul>"
+    return HttpResponse(response_data)
 
 
 def months(request, month):
+    """_summary_: recieves a month and returns the corresponding month.
+
+    Args:
+        request (): request object
+        month (months_list element): month
+
+    Returns:
+        HttpResponse: response object
+    """
     if month in months_list:
-        return HttpResponse(f"You're looking at the {month} challenge.\n {months_challenge_text[months_list.index(month)]}")
+        return HttpResponse(f"<h1>You're looking at the {month} challenge.\n {months_list[month]}</h1>")
+    else:
+        return HttpResponseNotFound("Month not found.")
+
+
+def months_by_num(request, month):
+    """_summary_:recieves a number of month and returns the corresponding month.
+
+    Args:
+        request (): request object
+        num (int): number of month
+
+    Returns:
+        HttpResponse: response object
+    """
+    months = list(months_list.keys())
+    if month in range(1, months.__len__() + 1):
+        # get the redirect url path
+        redirect_path = reverse('challenges_path', kwargs={
+                                'month': months[month - 1]})
+        return HttpResponseRedirect(redirect_path)
     else:
         return HttpResponseNotFound("Month not found.")
